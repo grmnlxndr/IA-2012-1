3504b8bfa750a8e169da6b9222ac04e03a4ca315
German <German@German-DELL>
2012-09-11 16:43:56 -0300

    Se borro lo que estaba al pedo.

--- TPIIAAG/src/tpiiaag/TPIIAAG.java
+++ TPIIAAG/src/tpiiaag/TPIIAAG.java
@@ -4,31 +4,17 @@
  */
 package tpiiaag;
 
-import javax.swing.JOptionPane;
-import javax.swing.LookAndFeel;
-import javax.swing.UIManager;
-import sun.swing.DefaultLayoutStyle;
-
 /**
  *
  * @author German
  */
 public class TPIIAAG {
 
-    /** nada
+    /**
+     * nada
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        try {
-            UIManager.setLookAndFeel(
-                    UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-        }
-
-
-        //RUBEN GIL DE MIERDA............................
-        JOptionPane.showMessageDialog(null, "Esteban se la come", "MENSAJE MUY IMPORTANTE", JOptionPane.INFORMATION_MESSAGE);
-        JOptionPane.showMessageDialog(null, "Rubén, no te hagas el vivo... Buscá la forma de eliminar el Branch!", "Más importante todavía", JOptionPane.ERROR_MESSAGE);
     }
 }
